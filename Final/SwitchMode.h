3e9aee58b85a9eba92674d2a7ead08a8fceef110
Tomoooji <tomoji.k.2nd@gmail.com>
2026-03-10 16:19:31 +0900

    .

--- Final/SwitchMode.h
+++ Final/SwitchMode.h
@@ -6,29 +6,29 @@
   constexpr char MAC_PS4CON[] = "e4:65:b8:d8:d4:80";
   constexpr float range_othogonal = radians(25); // 前後左右に±50°,斜めは±40°
   constexpr int range_ignoreLstick = 40;
-  constexpr int line_RL2pushed = 50;
+  constexpr int line_RL2pushed = 75;
   constexpr int range_ignoreRstick = 20;
 
   inline void PS4Input(){
-    connection_flag = PS4.isConnected();
-    disconnect_button = PS4.Touchpad();//
+    disconnect_button = PS4.Touchpad()? !disconnect_button: disconnect_button;//
+    connection_flag = PS4.isConnected() && !disconnect_button;
     appeal_button = PS4.Share() && PS4.Options();
     leg_joystick_x = PS4.LStickX();
     leg_joystick_y = PS4.LStickY();
     leg_button_R = PS4.R2Value() > line_RL2pushed;
     leg_button_L = PS4.L2Value() > line_RL2pushed;
-    leg_button_shift = PS4.L3();
+    leg_button_shift = PS4.Share() && !PS4.Options();
     yagura_L = PS4.Left();
     yagura_R = PS4.Right();
-    arm_joystick_x = PS4.RStickX();
+    arm_joystick_x = -PS4.RStickX();
     arm_joystick_y = PS4.RStickY();
     arm_button_UP =PS4.Circle();
     arm_button_DOWN =PS4.Cross();
     arm_button_init = PS4.Square() && PS4.Triangle();
     arm_button_pick = PS4.Square() && !PS4.Triangle();
     arm_button_drop = !PS4.Square() &&PS4.Triangle();
-    finger_button_UP = PS4.L1();
-    finger_button_DOWN=PS4.R1();
+    finger_button_UP = PS4.R1();
+    finger_button_DOWN=PS4.L1();
     clow_button_UP = PS4.Up();
     clow_button_DOWN = PS4.Down();
   }
