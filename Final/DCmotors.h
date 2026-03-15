d621dc2fccacc00845a709546260e72f5f4edb81
Tomoooji <tomoji.k.2nd@gmail.com>
2026-03-15 13:16:22 +0900

    refactoring...

--- Final/DCmotors.h
+++ Final/DCmotors.h
@@ -1,27 +1,30 @@
-extern constexpr int DCpins[];
-extern constexpr float leg_motor_gains[]
-extern constexpr int leg_accel;
-extern enum SPEED_OPTIONS{
+//constexpr int DC_default_speed = 150; //             櫓用アームの回転速度(足回りは255)[n/255]
+//constexpr int leg_speed_high = 200; //             櫓用アームの回転速度(足回りは255)[n/255]
+//constexpr int leg_speed_low = 85; //             櫓用アームの回転速度(足回りは255)[n/255]
+enum SPEED_OPTIONS{
   SLOW = 85,
   MIDDLE = 100,
   HIGH = 200,
   YAGURA = 100
 };
+constexpr float leg_motor_gains[] = {1, 1, 1, 1}; // 足回りモーターの速度定数[最大のn倍]
+extern constexpr int leg_accel = 5;
 
 
-struct DCmotor{
- public:
-  //*
-  enum MOTORID{
+
+enum MOTORID{
   FRONTRIGHT, //0
   BACKRIGHT,  //1
   BACKLEFT,   //2
   FRONTLEFT,  //3
   YAGURAARM   //4
-  };
-  //*/
+};
+
+struct DCmotor{
+ public:
+  const int NUM;
   int leg_direc1, leg_direc2;
-  int current_speed[5] = {0,0,0,0,0};
+  int speed[NUM];
 
   void begin(){
     for(int pin:DCpins){
@@ -32,21 +35,30 @@ struct DCmotor{
 
   void driveAll(){
     for(int idx=0; idx<5; idx++){
-      ledcWrite(DCpins[idx*2], (this->current_speed[idx]>0? current_speed[idx]: 0));
-      ledcWrite(DCpins[idx*2+1], (this->current_speed[idx]<0? -current_speed[idx]: 0));
+      ledcWrite(DCpins[idx*2], (this->speed[idx]>0? constrain(speed[idx], 0, 255): 0));
+      ledcWrite(DCpins[idx*2+1], (this->speed[idx]<0? constrain(-speed[idx], 0, 255): 0));
     }
   }
 
   bool addAccel(int idx, int target){
-    current_speed[idx] = target-current_speed[idx]>leg_accel? current_speed[idx] + leg_accel: target;
-    return current_speed[idx] == target;
+    speed[idx] = target-speed[idx]>leg_accel? speed[idx] + leg_accel: target;
+    return speed[idx] == target;
   }
 };
 
-inline void calcOmuni(DCmotor& leg, int direcX, int direcY, int turn, int speedVal){
-  leg.current_speed[DCmotor::FRONTRIGHT] = speedVal*sign(direcY -direcX -turn)*leg_motor_gains[DCmotor::FRONTRIGHT]
-  leg.current_speed[DCmotor::BACKRIGHT] = speedVal*sign(direcY +direcX -turn)*leg_motor_gains[DCmotor::BACKRIGHT]
-  leg.current_speed[DCmotor::BACKLEFT] = speedVal*sign(direcY -direcX +turn)*leg_motor_gains[DCmotor::BACKLEFT]
-  leg.current_speed[DCmotor::FRONTLEFT] = speedVal*sign(direcY +direcX +turn)*leg_motor_gains[DCmotor::FRONTLEFT]
-  //addAccel(leg.current_speed[leg::FRONTRIGHT], speedVal*sign(direcY -direcX -turn)*leg_motor_gains[leg::FRONTRIGHT])
+inline void setDirection()
+
+inline void calcOmuni(DCmotor& leg, int direcX, int direcY, int turn, int speed_l, int speed_r, bool useaccel = false){
+  if(!useaccel){
+    leg.speed[DCmotor::FRONTRIGHT] = (speed_l*sign(direcY -direcX)) - (speed_r*turn) *leg_motor_gains[DCmotor::FRONTRIGHT]
+    leg.speed[DCmotor::BACKRIGHT]  = (speed_l*sign(direcY +direcX)) - (speed_r*turn) *leg_motor_gains[DCmotor::BACKRIGHT]
+    leg.speed[DCmotor::BACKLEFT]   = (speed_l*sign(direcY -direcX)) + (speed_r*turn) *leg_motor_gains[DCmotor::BACKLEFT]
+    leg.speed[DCmotor::FRONTLEFT]  = (speed_l*sign(direcY +direcX)) + (speed_r*turn) *leg_motor_gains[DCmotor::FRONTLEFT]
+  }
+  else{
+    addAccel(leg.speed[DCmotor::FRONTRIGHT],(speed_l*sign(direcY -direcX)) - (speed_r*turn) *leg_motor_gains[DCmotor::FRONTRIGHT]);
+    addAccel(leg.speed[DCmotor::BACKRIGHT], (speed_l*sign(direcY +direcX)) - (speed_r*turn) *leg_motor_gains[DCmotor::BACKRIGHT]);
+    addAccel(leg.speed[DCmotor::BACKLEFT] , (speed_l*sign(direcY -direcX)) + (speed_r*turn) *leg_motor_gains[DCmotor::BACKLEFT]);
+    addAccel(leg.speed[DCmotor::FRONTLEFT], (speed_l*sign(direcY +direcX)) + (speed_r*turn) *leg_motor_gains[DCmotor::FRONTLEFT]);
+  }
 }
--- Final/IKArm.h
+++ Final/IKArm.h
@@ -1,8 +1,21 @@
 #pragma once
 
 #include <PCA9685.h>
-
 #include <Arduino.h>
+
+constexpr int SERVOMAX = 470, SERVOMIN = 120; //     PCA9685に渡す周波数のレンジ(基本変更しない)
+constexpr float arm_speed = 1;//1.25 //              アーム手首位置の移動速度[n*0.6cm(←目安)]
+constexpr int wrist_speed = 3; //                    アーム手首角度の回転速度[degree]
+constexpr int finger_speed = 6; //                   ハンドの回転速度[deg+ree]
+constexpr int clow_speed = 6; //                     ワーク引きずる爪の速度[degree]
+
+constexpr float arm_pos_init[] = { 10,  10, PI}; // アーム待機位置x,yと手首角度[radian]
+constexpr float arm_pos_pick[] = {162.24/4, -89.44/4, radians(135)}; // ワーク拾う位置x,yと手首角度[radian]
+constexpr float arm_pos_drop[] = {147.24/4, 152.36/4, 0.5}; // ワークセット位置x,yと手首角度[radian]
+
+constexpr int ArmLength[] = {36, 26, 21}; // アーム関節長さ(3個目は不使用)
+constexpr int InitalAngle[] = {0, 0, 90}; // アームのサーボ取付角度(完全折り畳み時が0°)
+
 
 constexpr int finger_min = 40, finger_max = 140; // degrees
 constexpr int clow_min = 40, clow_max = 140;
@@ -13,7 +26,6 @@ inline float clip2pi(float ang){
 
 struct IK2{
   const int* jointLength;
-  const int* initAngle;
   int jointLength_sq[2];
   float wrist[2];
   float elbow[2];
@@ -41,9 +53,19 @@ inline void calcAngle(IK2& ik){
   ),-1.0f,1.0f)));
 }
 
-inline void getWrist(IK2& ik, float* _result){}
+inline void getWrist(IK2& ik, float& _result){
+  _result[0] = 0.0f; _result[1] = 0.0f;
+  float ang = 0.0;
+  for(int i=0; i<2; i++){
+    ang += i-PI;
+    _result[0] += ik.armLength[i] *cos(ik.jointAngle[i]);
+    _result[1] += ik.armLength[i] *sin(ik.jointAngle[i]);
+  }
+}
 
-inline void ajastAngle(IK2& ik, int idx, float da){}
+inline void ajastAngle(IK2& ik, int idx, float da){
+  ik.jointAngle[idx] += da;
+}
 
 inline void InitIK(IK2& ik){
   setWrist(ik);
@@ -56,12 +78,18 @@ inline bool isWristOK(IK2& ik){return ik.distance > ik.jointLength[0] + ik.joint
 
 class Arm{
  public:
+  enum SERVOID{
+    SHOULDER,
+    ELBOW,
+    WRIST,
+    FINGER,
+    CLOW
+  };
   const int* initAngle;
   int servoAngle[5];
   IK2 Ik;
   PCA9685 pwm = PCA9685(0x40);
-  Arm(const int Lengths[], const int Angles_Init[]):
-    initAngle(Angles_Init), Ik(Lengths) {}
+  Arm(const int Lengths[], const int Angles_Init[]): Ik(Lengths) {}
   
   void begin(bool isTest = false){
     if(!isTest){
@@ -96,6 +124,11 @@ class Arm{
     this->servoAngle[4] = constrain(this->servoAngle[4] +da, clow_min, clow_max);
   }
   
+  void ajastIk(int idx, int da){
+    this->Ik.jointAngle[idx] = radians(this->servoAngle[idx] +da) + this->initAngle[i];
+    getWrist(this->Ik, this->Ik.wriet);
+  }
+
   void updateServos(){
     // don't use for to ajast set direction
     this->pwm.setPWM(Channels[0],0,map(180-constrain(this->servoAngle[0], 0, 180), 0, 180, SERVOMIN, SERVOMAX));
